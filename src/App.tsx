a579df883be7468b8be5222c579c8a4d6098be22
Joan D <joandidachr@gmail.comm>
2026-01-28 06:58:33 +0100

    Fix GitHub Pages routing by adding basename to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Mi-Dentista">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/" element={<Index />} />
